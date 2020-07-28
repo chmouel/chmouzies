f6ac0d9be4ae9396572255532d981b0b6146157c
Chmouel Boudjnah <chmouel@chmouel.com>
2020-07-28 11:06:24 +0200

    Add tekton script template: https://blog.chmouel.com/2020/07/28/tekton-yaml-templates-and-script-feature/
    Signed-off-by: Chmouel Boudjnah <chmouel@chmouel.com>

---
+++ work/tekton-script-template.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -eu
+fname=${1:-template.yaml}
+
+TMP=$(mktemp /tmp/.mm.XXXXXX)
+clean() { rm -f ${TMP}; }
+trap clean EXIT
+
+cat ${fname} > ${TMP}
+
+cd $(dirname $(readlink -f ${0}))
+
+
+oifs=${IFS}
+IFS="
+"
+for line in $(grep "## INSERT" ${fname});do
+    F2=$(<${TMP})
+
+    scriptfile=${line//## INSERT /}
+    scriptfile=${scriptfile//[ ]/}
+    echo "'${scriptfile}'"
+    [[ -e ${scriptfile} ]] || { echo "Could not find ${scriptfile}"; continue ;}
+    indentation="$(grep -B1 ${line} template.yaml|head -1|gsed 's/^\([ ]*\).*/\1/')"
+    indentation="${indentation}    "
+    F1=$(sed "s/^/${indentation}/" ${scriptfile})
+    cat <(echo "${F2//${line}/$F1}") > ${TMP}
+done
+
+cat ${TMP}
