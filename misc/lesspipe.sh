4e25777cccde85e8c896b804d65091b8b34894a3
Chmouel Boudjnah <chmouel@chmouel.com>
2020-09-25 07:43:08 +0200

    Every tool for highlights

---
+++ misc/lesspipe.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+
+for source in "$@"; do
+    case $source in
+	*ChangeLog|*changelog) 
+        source-highlight --failsafe -f esc --lang-def=changelog.lang --style-file=esc256.style -i "$source" ;;
+	*Makefile|*makefile) 
+        source-highlight --failsafe -f esc --lang-def=makefile.lang --style-file=esc256.style -i "$source" ;;
+	*.tar|*.tgz|*.gz|*.bz2|*.xz)
+        lesspipe.sh "$source" ;;
+    *.json)
+        jq -C '.' ${source};;
+    *.md|*.yaml)
+        pygmentize -O colorful  "$source";;
+    # *) source-highlight --failsafe --infer-lang -f esc --style-file=esc.style -i "$source" ;;
+    *) bat --color=always --style=snip ${source} ;;
+    esac
+done
