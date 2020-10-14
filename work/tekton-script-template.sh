02917d5e434fac880fa1c8dddd2c87168944a587
Chmouel Boudjnah <chmouel@chmouel.com>
2020-10-14 08:46:36 +0200

    don't apply indentation on empty lines or yamllint bomb it

--- work/tekton-script-template.sh
+++ work/tekton-script-template.sh
@@ -4,28 +4,27 @@ set -eu
 fname=${1:-template.yaml}
 
 TMP=$(mktemp /tmp/.mm.XXXXXX)
-clean() { rm -f ${TMP}; }
+clean() { rm -f "${TMP}"; }
 trap clean EXIT
 
-cat ${fname} > ${TMP}
+cat "${fname}" > ${TMP}
 
-cd $(dirname $(readlink -f ${fname}))
+cd "$(dirname "$(readlink -f "${fname}")")"
 
 
-oifs=${IFS}
 IFS="
 "
 
-for line in $(grep "## INSERT" $(basename ${fname}));do
-    F2=$(<${TMP})
+for line in $(grep "## INSERT" "$(basename "${fname}")");do
+    F2=$(<"${TMP}")
 
     scriptfile=${line//## INSERT /}
     scriptfile=${scriptfile//[ ]/}
     [[ -e ${scriptfile} ]] || { echo "Could not find ${scriptfile}"; continue ;}
-    indentation="$(grep -B1 ${line} template.yaml|head -1|sed 's/^\([ ]*\).*/\1/')"
-    indentation="${indentation}    "
-    F1=$(sed "s/^/${indentation}/" ${scriptfile})
-    cat <(echo "${F2//${line}/$F1}") > ${TMP}
+    indentation="$(grep -B1 "${line}" template.yaml|head -1|sed 's/^\([ ]*\).*/\1/')"
+    indentation="${indentation}  "
+    F1=$(sed -e "s/^/${indentation}/" "${scriptfile}"|sed 's/^\s*$//')
+    cat <(echo "${F2//${line}/$F1}") > "${TMP}"
 done
 
-cat ${TMP}
+cat "${TMP}"
