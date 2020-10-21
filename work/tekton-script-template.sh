48f035421e94470b4a2b1e012065ea09d6de209d
Chmouel Boudjnah <chmouel@chmouel.com>
2020-10-21 11:13:35 +0200

    bug fixes

--- work/tekton-script-template.sh
+++ work/tekton-script-template.sh
@@ -15,11 +15,12 @@ IFS="
 "
 grep "## INSERT" "$(basename "${fname}")"|while read -r line;do
     F2=$(<"${TMP}")
-
     scriptfile=${line//## INSERT /}
     scriptfile=${scriptfile//[ ]/}
     [[ -e ${scriptfile} ]] || { echo "Could not find ${scriptfile}"; continue ;}
+    # Grep the current indentation of where we have our INSERT
     indentation="$(grep -B1 "${line}" template.yaml|head -1|sed 's/^\([ ]*\).*/\1/')"
+    # Add two space to that indentation
     indentation="${indentation}  "
     F1=$(sed -e "s/^/${indentation}/" "${scriptfile}"|sed 's/^\s*$//')
     cat <(echo "${F2//${line}/$F1}") > "${TMP}"
