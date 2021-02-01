d67b03bc8fc78bc2f29a23ba6c4883a6ee7a028d
Chmouel Boudjnah <chmouel@chmouel.com>
2021-02-01 17:44:28 +0100

    Finally does what i had on osx :love:

--- misc/wmctrl-resize-and-center.sh
+++ misc/wmctrl-resize-and-center.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # resizes the window to full height and 50% width and moves into the center of the secreen
-# slightly adapted from to make it to the center https://unix.stackexchange.com/a/53228
+# adapted from https://unix.stackexchange.com/a/53228 so we center the window by
+# default unless we have the rotate argument and then we rotate the window
+# around the screen from 1/3 => 2/3 => 3/3
 
 #define the height in px of the top system-bar:
 TOPMARGIN=27
@@ -12,16 +14,36 @@ RIGHTMARGIN=10
 SCREEN_WIDTH=$(xwininfo -root | awk '$1=="Width:" {print $2}')
 SCREEN_HEIGHT=$(xwininfo -root | awk '$1=="Height:" {print $2}')
 
+PREVIOUS=$HOME/.cache/rotatewindow/cache
+[[ -d $(dirname ${PREVIOUS}) ]] || mkdir -p $(dirname ${PREVIOUS})
+
+[[ ${1} == "rotate" ]] && rotatearound=true
+
+eval $(xwininfo -id $(xdotool getactivewindow) |
+    sed -n -e "s/^ \+Absolute upper-left X: \+\([0-9]\+\).*/currentx=\1/p" \
+           -e "s/^ \+Absolute upper-left Y: \+\([0-9]\+\).*/currenty=\1/p" \
+           -e "s/^ \+Width: \+\([0-9]\+\).*/currentw=\1/p" \
+           -e "s/^ \+Height: \+\([0-9]\+\).*/currenth=\1/p" )
+
 # new width and height
 W=$(( $SCREEN_WIDTH / 2 - $RIGHTMARGIN ))
 H=$(( $SCREEN_HEIGHT - 2 * $TOPMARGIN ))
 
-# X, change to move left or right:
-
 # moving to the center
-X=$(( $SCREEN_WIDTH / 4 ))
-# moving to the left:
-#X=0; 
+X=$(( $SCREEN_WIDTH / 3 ))
+
+if [[ ${currentx} > ${X} ]];then
+    echo "droite" > ${PREVIOUS}
+elif [[ ${X} > ${currentx} ]];then
+    echo "gauche" > ${PREVIOUS}
+elif [[ -n ${rotatearound} && -e /tmp/previous ]]; then
+    previous=$(cat ${PREVIOUS})
+    if [[ ${previous} == "gauche" ]];then
+        X=${SCREEN_WIDTH}
+    elif [[ ${previous} == "droite" ]];then
+        X=0
+    fi
+fi
 
 Y=$TOPMARGIN
 
