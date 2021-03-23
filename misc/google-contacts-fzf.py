1a4024ec99f10cc5a1472e7ff6fb9573b0f8020a
Chmouel Boudjnah <chmouel@chmouel.com>
2021-03-23 09:25:41 +0100

    add google-contacts with fzf

---
+++ misc/google-contacts-fzf.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Author: Chmouel Boudjnah <chmouel@chmouel.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+import argparse
+import json
+import os
+import subprocess
+import tempfile
+
+jeez = json.load(open("/tmp/a.json"))
+
+
+def show_names():
+    allnames = []
+    for i in jeez:
+        if 'names' in i:
+            for names in i['names']:
+                name = names['displayName']
+                if name.startswith("+") or name[0].isdigit():
+                    continue
+                allnames.append(names['displayName'])
+    allnames.sort()
+    tmp = tempfile.NamedTemporaryFile(mode='w',
+                                      delete=False,
+                                      prefix="fzf-contact-")
+    tmp.write("\n".join(allnames))
+    tmp.close()
+    cmdline = f"cat {tmp.name}|fzf -0 -n 1 -m -1"
+    try:
+        popo = subprocess.run(cmdline,
+                              shell=True,
+                              stdout=subprocess.PIPE,
+                              check=True)
+    except subprocess.CalledProcessError:
+        return ""
+    finally:
+        os.remove(tmp.name)
+
+    reply = popo.stdout.decode().strip()
+    preview(reply)
+
+
+def preview(name):
+    cmdline = f"goobook dquery '{name}'"
+    try:
+        popo = subprocess.run(cmdline,
+                              shell=True,
+                              stdout=subprocess.PIPE,
+                              check=True)
+    except subprocess.CalledProcessError as error:
+        print(error)
+        return ""
+
+    print(popo.stdout.decode().strip())
+    answer = input("Copy (n)ame (e)mail (m)obile (a)addres")
+    print(answer)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument("function", nargs="?")
+    parser.add_argument("args", nargs="*")
+    args = parser.parse_args()
+    if args.function == "preview":
+        preview(args.args)
+    else:
+        show_names()
