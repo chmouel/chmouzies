c2fa3deffbb4ae2a0712c82735a03f6d09a0a471
Chmouel Boudjnah <chmouel@chmouel.com>
2021-03-25 16:43:40 +0100

    more changes!

--- a.el
+++ a.el
@@ -7,28 +7,36 @@
   (magit-completing-read prompt (magit-list-remotes)
                          (lambda (elt) (not (string= elt exclude)))))
 
-(defun ppr-make-pull-request ()
-  (interactive)
-  (let* ((origin-remote (magit-read-remote "Base Remote"))
-         (github-origin-remote (replace-regexp-in-string
+(defun ppr-make-pull-request (origin-remote user-remote &optional origin-remote-branch pushforce)
+  (interactive
+   (list (magit-read-remote "Base Remote")
+         (magit-read-remote "User Remote")))
+  (let* ((github-origin-remote (replace-regexp-in-string
                                 "\\(https://github.com/\\\|git@github.com:\\)" ""
                                 (magit-git-string "remote" "get-url" origin-remote)))
-         (user-remote (ppr-magit-read-remote "User Remote" origin-remote))
+
          (github-user-remote (replace-regexp-in-string
                               "\\(https://github.com/\\\|git@github.com:\\)" ""
                               (magit-git-string "remote" "get-url" user-remote)))
          (current-branch (magit-get-current-branch))
          (origin-remote-branch
-          (ppr-magit-read-remote-branch
-           (format "Base remote branch on %s" origin-remote)
-           origin-remote)))
-
-    (if (yes-or-no-p
-         (format "Push force current branch %s to user remote %s" current-branch user-remote))
-        (magit-run-git-async
+          (if origin-remote-branch
+              origin-remote-branch
+            (ppr-magit-read-remote-branch
+             (format "Base remote branch on %s" origin-remote)
+             origin-remote))))
+    (if (or pushforce
+            (yes-or-no-p
+             (format
+              "Push force current branch %s to user remote %s"
+              current-branch
+              user-remote)))
+        (magit-run-git
          "push" "-f" "-v"
          user-remote current-branch))
 
     (with-editor (magit-shell-command
                   (format "hub pull-request -b %s:%s -h %s:%s" github-origin-remote
                           origin-remote-branch github-user-remote current-branch)))))
+
+(ppr-make-pull-request "origin" "chmouel" "master" t)
